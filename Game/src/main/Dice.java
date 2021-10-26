da49dca95444c4279fc0d1c14123677b0a5e1ebb
AugustHjortholm <s215800@dtu.dk>
2021-10-26 07:57:20 +0200

    removed Dice.setDie1 and Dice.setDie2 since those methods were initially implemented for testing purposes, but since the value of the dice is only evaluated in the game using Dice.getSum, the method can be replaced by an int, which means Dice.setDie1 and Dice.setDie2 were no longer important

--- Game/src/main/Dice.java
+++ Game/src/main/Dice.java
@@ -26,13 +26,5 @@ public class Dice {
     public int getDie2() {
         return this.die2;
     }
-    //Should we remove the get setDie1 and setDie2 and replace them with setSum since nothing in the game cares about the separate dies but cares only for their sum.
-    public void setDie1(int die1) {
-        this.die1 = die1;
-    }
-
-    public void setDie2(int die2) {
-        this.die2 = die2;
-    }
 }
 
--- Game/test/main/GameBoardTest.java
+++ Game/test/main/GameBoardTest.java
@@ -29,4 +29,72 @@ class GameBoardTest {
         GUI_Player activePlayer2 = gameboard.activePlayer;
         assertNotSame(activePlayer1, activePlayer2);
     }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 2 is rolled")
+    void getFieldValueOnRoll2Test() {
+        assertEquals(gameboard.getToFieldValue(2),250);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 3 is rolled")
+    void getFieldValueOnRoll3Test() {
+        assertEquals(gameboard.getToFieldValue(3),-100);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 4 is rolled")
+    void getFieldValueOnRoll4Test() {
+        assertEquals(gameboard.getToFieldValue(4),100);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 5 is rolled")
+    void getFieldValueOnRoll5Test() {
+        assertEquals(gameboard.getToFieldValue(5),-20);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 6 is rolled")
+    void getFieldValueOnRoll6Test() {
+        assertEquals(gameboard.getToFieldValue(6),180);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 7 is rolled")
+    void getFieldValueOnRoll7Test() {
+        assertEquals(gameboard.getToFieldValue(7),0);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 8 is rolled")
+    void getFieldValueOnRoll8Test() {
+        assertEquals(gameboard.getToFieldValue(8),-70);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 9 is rolled")
+    void getFieldValueOnRoll9Test() {
+        assertEquals(gameboard.getToFieldValue(9),60);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 10 is rolled")
+    void getFieldValueOnRoll10Test() {
+        assertEquals(gameboard.getToFieldValue(10),-80);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 11 is rolled")
+    void getFieldValueOnRoll11Test() {
+        assertEquals(gameboard.getToFieldValue(11),-50);
+    }
+
+    @Test
+    @DisplayName("Tests if the correct value is added to the score when 12 is rolled")
+    void getFieldValueOnRoll12Test() {
+        assertEquals(gameboard.getToFieldValue(12),650);
+    }
+
+
 }
